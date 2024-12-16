4c7758423bea14adb3db404f7c9c2ba634b378c1
rectanglehihat <rectanglehihat@gmail.com>
2024-12-17 00:16:47 +0900

    Fix: api 주소 수정

--- src/services/push-notification.ts
+++ src/services/push-notification.ts
@@ -42,7 +42,7 @@ export const togglePushNotificationOnServer = async () => {
       }
     };
     const response = await fetchExtendedAuth(
-      'members/notifications',
+      '/members/notifications',
       requestOptions
     );
 
